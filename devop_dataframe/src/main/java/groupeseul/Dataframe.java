f1e2fcf9904e98d4b15e2949819f77fd8a117568
hacenesuper <haceneamrane22@gmail.com>
2023-04-19 13:12:38 +0200

    colone

---
+++ devop_dataframe/src/main/java/groupeseul/Dataframe.java
@@ -0,0 +1,9 @@
+package groupeseul;
+
+import java.util.ArrayList;
+
+public class Dataframe {
+    private ArrayList<String> type;
+    private ArrayList<ArrayList<String>> values;
+    private ArrayList<String> labels;
+}
---
+++ devop_dataframe/src/main/java/groupeseul/col.java
@@ -0,0 +1,82 @@
+package groupeseul;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class col <VALUE extends Object>{
+    private String label;
+    private String type;
+    private List<VALUE> values;
+
+
+    /**
+     * creation d'une colone dune liste de valeurs 
+     * @param label le titre de la colone 
+     * @param type le type des valeurs de la colone 
+     * @param values liste des valeurs de la colone 
+     */
+    public col(String label, String type, List<VALUE> values){
+        this.label = label;
+        this.type = type;
+        this.values = values;
+    }
+
+    /**
+     * creation d'une colone vide 
+     * @param label le titre de la colone 
+     * @param type le type de la colone 
+     */
+    public col(String label, String type){
+        this.label = label;
+        this.type = type;
+        this.values = new ArrayList<VALUE>();
+    }
+/**
+     * donne la taille de la colone 
+     * @return taille de la colone 
+     */
+    public int getsize(){
+        return this.values.size();
+    }
+    
+    /**
+     * la methode donne le label de la colone 
+     * @return le label de la colone 
+     */
+    public String getLabel() {
+        return label;
+    }
+    
+    /**
+     * la methode retourne le type de la colone 
+     * @return le ltype de la colone 
+     */
+    public String getType() {
+        return type;
+    }
+    
+    /**
+     * This methode is used to get the values of a Column object
+     * @return une liste des valeurs presents dans la colone 
+     */
+    public List<VALUE> getValues() {
+        return values;
+    }
+    /**
+     * This methode is used to test whether the Column contains a value
+     * @param v la valeur a virifier si elle est presente dans la colone 
+     * @return true si la valeur est presente , faux sinon 
+     */
+    public boolean present (VALUE v){
+        return values.contains(v);
+        
+    }
+    
+    /**
+     * la methode ajoute une valeur a la colone 
+     * @param val la valeur a ajouter 
+     */
+    public void add(VALUE v){
+        this.values.add(v);
+    }
+}
